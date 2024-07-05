6f275297bda6d7e78bb93154187e66a1331d0828
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-07-04 22:03:26 -0300

    feat(config.mjs): added skills (general and social) for register constants with localizations

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -21,3 +21,34 @@ shinobiNoSho.abilityAbbreviations = {
   wis: 'shinobiNoSho.Ability.Wis.abbr',
   cha: 'shinobiNoSho.Ability.Cha.abbr',
 };
+
+shinobiNoSho.skills = {
+	geral: {
+		acrobacia: "shinobiNoSho.Skills.geral.acrobacia",
+		arte: "shinobiNoSho.Skills.geral.arte",
+		atletismo: "shinobiNoSho.Skills.geral.atletismo",
+		ciencias: "shinobiNoSho.Skills.geral.ciencias",
+		cultura: "shinobiNoSho.Skills.geral.cultura",
+		ocultismo: "shinobiNoSho.Skills.geral.ocultismo",
+		concentracao: "shinobiNoSho.Skills.geral.concentracao",
+		disfarces: "shinobiNoSho.Skills.geral.disfarces",
+		escapar: "shinobiNoSho.Skills.geral.escapar",
+		furtividade: "shinobiNoSho.Skills.geral.furtividade",
+		animais: "shinobiNoSho.Skills.geral.animais",
+		mecanismos: "shinobiNoSho.Skills.geral.mecanismos",
+		medicina: "shinobiNoSho.Skills.geral.medicina",
+		prestidigitacao: "shinobiNoSho.Skills.geral.prestidigitacao",
+		procurar: "shinobiNoSho.Skills.geral.procurar",
+		prontidao: "shinobiNoSho.Skills.geral.prontidao",
+		rastrear: "shinobiNoSho.Skills.geral.rastrear",
+		veneficio: "shinobiNoSho.Skills.geral.veneficio"
+	},
+	social: {
+		atuacao: "shinobiNoSho.Skills.social.atuacao",
+		mudarAtitude: "shinobiNoSho.Skills.social.mudarAtitude",
+		barganha: "shinobiNoSho.Skills.social.barganha",
+		blefar: "shinobiNoSho.Skills.social.blefar",
+		intimidacao: "shinobiNoSho.Skills.social.intimidacao",
+		obterInfo: "shinobiNoSho.Skills.social.obterInfo",
+	}
+};
