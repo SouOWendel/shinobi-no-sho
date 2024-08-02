5bbcb4b453571cfc34cefcc127e30c7ddc1a9c4c
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-08-01 21:11:52 -0300

    feat(d8roll): added d8roll class (extended for foundry Roll class) with some changes to display system rolls with critical, degree, bonus, and rollmode

---
+++ module/dice/dice.mjs
@@ -0,0 +1,43 @@
+export async function d8Roll({
+  parts=[], data={}, event, critical=15, fumble=3,
+  fastForward, chooseModifier=false, template, title, dialogOptions,
+  chatMessage=true, messageData={}, rollMode, flavor
+}={}) {
+
+  // Handle input arguments
+  const formula = ["2d8"].concat(parts).join(" + ");
+  const defaultRollMode = rollMode || game.settings.get("core", "rollMode");
+
+  // Construct the D8Roll instance
+  const roll = new CONFIG.Dice.D8Roll(formula, data, {
+    flavor: flavor || title,
+    defaultRollMode,
+    rollMode,
+    critical,
+    fumble,
+  });
+
+	// Variável com os dados do Dialog
+  const configured = await roll.configureDialog({
+		title,
+		chooseModifier,
+		defaultRollMode,
+		defaultAbility: data?.item?.ability || data?.defaultAbility,
+		template
+	}, dialogOptions);
+	if (configured === null) return null;
+
+  // Evaluate the configured roll
+  await roll.evaluate({ 
+		allowInteractive: (roll.options.rollMode ?? defaultRollMode) !== CONST.DICE_ROLL_MODES.BLIND 
+	});
+
+  // Attach original message ID to the message
+  messageData = foundry.utils.expandObject(messageData);
+  const messageId = event?.target.closest("[data-message-id]")?.dataset.messageId;
+	if ( messageId ) foundry.utils.setProperty(messageData, "flags.shinobinosho.originatingMessage", messageId);
+
+  // Create a Chat Message
+  if ( roll && chatMessage ) await roll.toMessage(messageData, {data: configured});
+  return roll;
+}
